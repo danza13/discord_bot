48b76594aac8ff5714253a0484966135ebe08134
danza13 <136966054+danza13@users.noreply.github.com>
2025-04-17 18:03:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
+# ------------ main.py ------------
+import os
 import discord
+
+# 1. Зчитуємо токен зі змінної середовища
+TOKEN = os.getenv('DISCORD_TOKEN')
+if not TOKEN:
+    raise RuntimeError("DISCORD_TOKEN не встановлений у середовищі!")
+
+# 2. Налаштовуємо Intents
+intents = discord.Intents.default()
+intents.message_content = True      # щоб працювало on_message
+intents.guilds = True                # щоб бот бачив сервери
+intents.members = False              # якщо не потрібні події для юзерів
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        # щоб бот не реагував на власні повідомлення
+        if message.author == self.user:
+            return
+        print(f'Message from {message.author}: {message.content}')
 
-client = MyClient()
-client.run('ТОКЕН')
+# 3. Створюємо клієнта з передачею intents
+client = MyClient(intents=intents)
+
+# 4. Запускаємо
+client.run(TOKEN)
